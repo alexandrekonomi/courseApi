d92023d2bd1ac005e526ccd95fda73a7d206927e
alexandrekonomi <alexandrekonomi1234@gmail.com>
2023-05-15 21:48:21 -0300

    add CRUD lesson api

---
+++ src/main/java/com/ead/course/dtos/LessonDto.java
@@ -0,0 +1,13 @@
+package com.ead.course.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LessonDto {
+    @NotBlank
+    private String title;
+    private String description;
+    @NotBlank
+    private String videoUrl;
+}
