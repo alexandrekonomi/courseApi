955c1869903f3129ff10e9ef2b6d8820f594b38d
alexandrekonomi <alexandrekonomi1234@gmail.com>
2023-05-30 10:45:08 -0300

    add in application yaml config logs

--- src/main/java/com/ead/course/clients/CourseClient.java
+++ src/main/java/com/ead/course/clients/CourseClient.java
@@ -7,7 +7,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
